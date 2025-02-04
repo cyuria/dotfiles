fd6a52e5557e5a4e543d138f4e5375526a753b1c
Cyuria <cyuria.dev@gmail.com>
2025-02-04 12:18:33 +1100

    feat(nvim): add gx.nvim plugin

--- config/nvim/lua/config/lazysetup/lazyutilitysetup.lua
+++ config/nvim/lua/config/lazysetup/lazyutilitysetup.lua
@@ -133,5 +133,15 @@ return {
                 end,
             },
         },
-    }
+    },
+    -- gx.nvim
+    {
+        "chrishrb/gx.nvim",
+        keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+        cmd = { "Browse" },
+        init = function() vim.g.netrw_nogx = 1 end,
+        dependencies = { "nvim-lua/plenary.nvim" },
+        submodules = false,
+        opts = { },
+    },
 }
