6a02f293d22c6500962c99cfcbe22cb8aca50368
Cyuria <cyuria.dev@gmail.com>
2025-02-04 12:18:46 +1100

    fix(nvim): specify correct neovim lua runtime version

--- config/nvim/lua/config/lspsetup.lua
+++ config/nvim/lua/config/lspsetup.lua
@@ -11,8 +11,11 @@ local customconfig = {
                     -- Make the server aware of Neovim runtime files
                     library = vim.list_extend(
                         vim.api.nvim_get_runtime_file("", true),
-                        { "${3rd}/luv/library" }
+                        { "${3rd}/luv/library", "${template}/jit" }
                     ),
+                },
+                runtime = {
+                    version = type(jit) == 'table' and "LuaJIT" or _VERSION,
                 },
             },
         },
