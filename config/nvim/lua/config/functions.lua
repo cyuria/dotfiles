e603320cb90e15698f88bc1996ce03a4e3d364e2
Cyuria <cyuria.dev@gmail.com>
2025-05-05 11:24:19 +1000

    fix(nvim): remove unused broken shell mappings

--- config/nvim/lua/config/functions.lua
+++ config/nvim/lua/config/functions.lua
@@ -1,31 +1,3 @@
--- Creates an interactive shell process
--- in the current buffer. Returns the job id
--- of the shell
-local function createShell(cmd)
-    local JID = vim.fn.termopen(vim.g.terminal)
-    if cmd then
-        vim.fn.chansend(JID, cmd)
-        vim.fn.chansend(JID, '\r')
-    end
-    return JID
-end
-
--- Map <c-b> to create a floating window and run createShell(cmd)
-local function floatingShell(cmd)
-    local stats = vim.api.nvim_list_uis()[1]
-    local winconfig = {
-        relative = "editor",
-        border = "rounded",
-        width = math.ceil(stats.width * 0.6),
-        height = math.ceil(stats.height * 0.6),
-        col = math.ceil(stats.width * 0.2),
-        row = math.ceil(stats.height * 0.2)
-    }
-    local buffer = vim.api.nvim_create_buf(true, true)
-    vim.api.nvim_open_win(buffer, true, winconfig)
-    return createShell(cmd)
-end
-
 local function reloadCtags()
     vim.system({ "/usr/bin/sh", "-c", "rg --files | ctags -f .tags --recurse -L -" })
 end
@@ -45,8 +17,6 @@ local function googleSearch(s_start, s_end)
 end
 
 return {
-    createShell = createShell,
-    floatingShell = floatingShell,
     reloadCtags = reloadCtags,
     googleSearch = googleSearch,
 }
--- config/nvim/lua/config/remap/functionmappings.lua
+++ config/nvim/lua/config/remap/functionmappings.lua
@@ -2,16 +2,6 @@ local functions = require'config.functions'
 
 -- Create mappings to load interactive shells in different
 -- window configurations
-vim.keymap.set('n', '<leader>sh', functions.createShell, {})
-vim.keymap.set('n', '<leader>ss', function ()
-    vim.cmd 'new'
-    functions.createShell()
-end, {})
-vim.keymap.set('n', '<leader>sv', function ()
-    vim.cmd 'vnew'
-    functions.createShell()
-end, {})
-vim.keymap.set('n', '<leader>sf', functions.floatingShell, {})
 vim.keymap.set('n', '<leader>?', function ()
     local old_opfunc = vim.go.operatorfunc
     _G.op_func_googlesearch = function ()
