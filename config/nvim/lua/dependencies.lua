1c1ef1cade22045d341b68da3c5cb32b6a44934f
Cyuria <cyuria.dev@gmail.com>
2025-05-07 23:57:29 +1000

    chore(nvim): bump config dependency checks to 0.11

--- config/nvim/lua/dependencies.lua
+++ config/nvim/lua/dependencies.lua
@@ -1,7 +1,7 @@
 local version = vim.version()
 
 return {
-    enable_plugins = version.major >= 1 or version.minor >= 8,
+    enable_plugins = version.major >= 1 or version.minor >= 11,
     enable_loader = version.major >= 1 or version.minor >= 10,
     enable_github = vim.fn.executable 'gh' == 1,
     enable_discord = vim.uv.fs_stat(vim.fs.joinpath(
