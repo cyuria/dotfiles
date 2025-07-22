d0844ec468d9ad9e88b5e5343f0b57f6446365ee
Cyuria <cyuria.dev@gmail.com>
2025-07-22 11:45:47 +1000

    feat(nvim): enable tinymist LSP when available

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -1,14 +1,15 @@
-vim.lsp.enable('clangd')
-vim.lsp.enable('zls')
-vim.lsp.enable('glsl-lsp')
+vim.lsp.enable("clangd")
+vim.lsp.enable("zls")
+vim.lsp.enable("glsl-lsp")
+vim.lsp.enable("tinymist")
 
-vim.lsp.config('*', { capabilities = require('blink.cmp').get_lsp_capabilities() })
+vim.lsp.config("*", { capabilities = require("blink.cmp").get_lsp_capabilities() })
 
-vim.lsp.config('glsl-lsp', {
-        cmd = { 'glsl-lsp' },
-        filetypes = { 'glsl' },
-        root_dir = function (fname)
-            return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-        end,
-        settings = {},
+vim.lsp.config("glsl-lsp", {
+	cmd = { "glsl-lsp" },
+	filetypes = { "glsl" },
+	root_dir = function(fname)
+		return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+	end,
+	settings = {},
 })
