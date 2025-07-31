b9f5f49b22b873306b5fafaf282b0c90f80bc346
Cyuria <cyuria.dev@gmail.com>
2025-07-31 22:48:26 +1000

    fix: enable treesitter with new main branch changes

--- config/nvim/lua/config/autocommands.lua
+++ config/nvim/lua/config/autocommands.lua
@@ -1,44 +1,58 @@
 local augroup = vim.api.nvim_create_augroup("openBufOpts", {})
 
 -- Repoen files with cursor in last location
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    group = augroup,
-    desc = 'return cursor to where it was last time closing the file',
-    pattern = '*',
-    command = 'silent! normal! g`"zv',
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	group = augroup,
+	desc = "return cursor to where it was last time closing the file",
+	pattern = "*",
+	command = 'silent! normal! g`"zv',
 })
 
-vim.api.nvim_create_autocmd({ 'Colorscheme' }, {
-    group = augroup,
-    desc = 'cache the neovim color scheme whenever it changes for persistence',
-    callback = function()
-        local file_path = vim.fn.stdpath('data') .. '/saved_colourscheme'
-        local file, open_err = vim.uv.fs_open(file_path, "w", 432)
-        if open_err ~= nil then
-            error(open_err .. " Could not open file '" .. file_path .. "' for writing to cache color scheme")
-        end
-        assert(file ~= nil)
-        local _, write_err = vim.uv.fs_write(file, vim.g.colors_name)
-        if write_err ~= nil then
-            error(write_err .. " Could not write to file '" .. file_path .. "' to cache color scheme")
-        end
-        local _, close_err = vim.uv.fs_close(file)
-        if close_err ~= nil then
-            error(close_err .. " Could not close file '" .. file_path .. "' to cache color scheme")
-        end
-    end,
+vim.api.nvim_create_autocmd({ "Colorscheme" }, {
+	group = augroup,
+	desc = "cache the neovim color scheme whenever it changes for persistence",
+	callback = function()
+		local file_path = vim.fn.stdpath("data") .. "/saved_colourscheme"
+		local file, open_err = vim.uv.fs_open(file_path, "w", 432)
+		if open_err ~= nil then
+			error(open_err .. " Could not open file '" .. file_path .. "' for writing to cache color scheme")
+		end
+		assert(file ~= nil)
+		local _, write_err = vim.uv.fs_write(file, vim.g.colors_name)
+		if write_err ~= nil then
+			error(write_err .. " Could not write to file '" .. file_path .. "' to cache color scheme")
+		end
+		local _, close_err = vim.uv.fs_close(file)
+		if close_err ~= nil then
+			error(close_err .. " Could not close file '" .. file_path .. "' to cache color scheme")
+		end
+	end,
 })
 
-if require 'config.dependencies'.enable then
-    vim.api.nvim_create_autocmd({ "BufLeave" }, {
-        group = augroup,
-        command = 'NvimcordUpdate',
-    })
-    vim.api.nvim_create_autocmd({ "DirChanged" }, {
-        group = augroup,
-        callback = function()
-            local wsname = require 'nvimcord.workspace'.get_name()
-            require 'nvimcord.discord'.config.workspace_name = wsname
-        end
-    })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = augroup,
+	callback = function()
+		local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+		if not pcall(vim.treesitter.language.inspect, lang) then
+			return
+		end
+
+		vim.treesitter.start()
+		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.bo.indentexpr = "v:la.require'nvim-tresitter'.indentexpr()"
+	end,
+})
+
+if require("config.dependencies").enable then
+	vim.api.nvim_create_autocmd({ "BufLeave" }, {
+		group = augroup,
+		command = "NvimcordUpdate",
+	})
+	vim.api.nvim_create_autocmd({ "DirChanged" }, {
+		group = augroup,
+		callback = function()
+			local wsname = require("nvimcord.workspace").get_name()
+			require("nvimcord.discord").config.workspace_name = wsname
+		end,
+	})
 end
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 -- All non default keybinds should start with <leader>, which is space
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
@@ -21,6 +21,10 @@ vim.opt.incsearch = true
 
 vim.opt.undofile = true
 
+vim.opt.foldlevelstart = 3
+vim.opt.foldmethod = "expr"
+vim.opt.foldminlines = 1
+
 -- Set the search path for :find and :cd
 vim.opt.path = ",,**2,../**2"
 vim.opt.cdpath = ",,../,**1"
@@ -28,12 +32,6 @@ vim.opt.cdpath = ",,../,**1"
 -- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
--- Treesitter based folding
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevelstart = 3
-vim.opt.foldmethod = "expr"
-vim.opt.foldminlines = 1
 
 -- Neovide stuff
 vim.g.neovide_hide_mouse_when_typing = true
@@ -45,25 +43,25 @@ vim.g.loaded_perl_provider = 0
 vim.g.c_syntax_for_h = 1
 
 -- Remove extra menu items from mouse right click menu
-vim.cmd.aunmenu('PopUp.How-to\\ disable\\ mouse')
-vim.cmd.aunmenu('PopUp.-2-')
+vim.cmd.aunmenu("PopUp.How-to\\ disable\\ mouse")
+vim.cmd.aunmenu("PopUp.-2-")
 
 vim.filetype.add({
-    extension = {
-        gltf = "json",
-        frag = "glsl",
-        vert = "glsl",
-        mesh = "glsl",
-        ino = "cpp",
-    }
+	extension = {
+		gltf = "json",
+		frag = "glsl",
+		vert = "glsl",
+		mesh = "glsl",
+		ino = "cpp",
+	},
 })
 
 vim.diagnostic.config({ virtual_lines = true })
 
 -- rustaceanvim takes options as arguments
-vim.g.rustaceanvim = { server = { cmd = { 'rust-analyzer' } } }
+vim.g.rustaceanvim = { server = { cmd = { "rust-analyzer" } } }
 
 -- Colourscheme stuff
-vim.g.gruvbox_contrast_dark = 'soft'
+vim.g.gruvbox_contrast_dark = "soft"
 vim.g.gruvbox_baby_telescope_theme = 1
 vim.g.gruvbox_baby_transparent_mode = 1
