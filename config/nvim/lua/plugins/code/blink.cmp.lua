383e2f1109847b03a9b3c6599ed36908bcf6bb3c
Cyuria <cyuria.dev@gmail.com>
2025-08-01 17:45:27 +1000

    fix(nvim): manually write out blink.cmp mappings

--- config/nvim/lua/plugins/code/blink.cmp.lua
+++ config/nvim/lua/plugins/code/blink.cmp.lua
@@ -12,7 +12,19 @@ return {
 		opts = {
 			keymap = {
 				preset = "none",
-				["<C-enter>"] = { "select_and_accept" },
+				["<C-e>"] = { "hide" },
+				["<C-y>"] = { "select_and_accept" },
+				["<C-CR>"] = { "select_and_accept" },
+
+				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+				["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+				["<C-k>"] = { "show_signature" },
 			},
 			completion = {
 				accept = { auto_brackets = { enabled = true } },
