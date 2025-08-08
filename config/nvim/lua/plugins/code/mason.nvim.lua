f96ac49c8f77ea8c4e178399aa1fdda1feb73fde
Cyuria <cyuria.dev@gmail.com>
2025-08-08 10:22:59 +1000

    fix(nvim): improve startup time by better lazy loading of mason

--- config/nvim/lua/plugins/code/mason.nvim.lua
+++ config/nvim/lua/plugins/code/mason.nvim.lua
@@ -1,22 +1,24 @@
 ---@type LazySpec
 return {
-    {
-        "mason-org/mason.nvim",
-        build = ":MasonUpdate",
-        event = { "BufReadPre", "BufNewFile" },
-        cmd = "Mason",
-        opts = {},
-        config = function()
-            require 'mason'.setup()
-            require 'mason-lspconfig'.setup()
-        end,
-        dependencies = {
-            "mason-org/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
-            "blink.cmp",
+	{
+		"mason-org/mason.nvim",
+		build = ":MasonUpdate",
+		cmd = "Mason",
+		opts = {},
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"blink.cmp",
 
-            "p00f/clangd_extensions.nvim",
-            "jose-elias-alvarez/typescript.nvim",
-        },
-    }
+			"p00f/clangd_extensions.nvim",
+			"jose-elias-alvarez/typescript.nvim",
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		event = { "VeryLazy" },
+		opts = {},
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
+	},
 }
