a79f8299c805a8b3269483c6d230c4b06dd0d3c4
Cyuria <cyuria.dev@gmail.com>
2025-08-15 22:58:03 +1000

    fix(nvim): use ltex+ lsp instead of ltex

---
+++ config/nvim/lsp/ltex_plus.lua
@@ -0,0 +1,31 @@
+return {
+	settings = {
+		ltex = {
+			language = "en-AU",
+			dictionary = {
+				["en-AU"] = {
+					"overridable",
+					"toolchain",
+					"makefile",
+					"makefiles",
+					"MSYS",
+					"nasm",
+					"MinGW",
+					"Cygwin",
+					"glibc",
+					"libc",
+					"wrasm",
+					"Uno",
+					"Cyuria",
+					"Heitmann",
+					"cyuria",
+				},
+			},
+			disabledRules = {
+				["en-AU"] = {
+					"COPYRIGHT",
+				},
+			},
+		},
+	},
+}
