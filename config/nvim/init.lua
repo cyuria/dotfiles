f7054ffb56eee2221f1919a683ca1a592df2c2d0
Cyuria <cyuria.dev@gmail.com>
2026-04-15 22:26:01 +1000

    fix(nvim): some minor config qol changes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,4 @@
+vim.g.mapleader = ' '
 vim.opt.exrc = true
 vim.opt.number = true
 vim.opt.path = { '.', './*', './**/*', '../**/*' }
@@ -5,6 +6,8 @@ vim.opt.scrolloff = 4
 vim.opt.shiftwidth = 0
 vim.opt.spelllang = { 'en_au', 'de' }
 vim.cmd.colorscheme('retrobox')
+
+vim.keymap.set('', '<leader>', '<nop>')
 
 vim.lsp.config('*', {
 	root_markers = { '.git' },
@@ -20,7 +23,8 @@ vim.lsp.enable('ty')
 vim.lsp.enable('zls')
 
 vim.pack.add({
-	'https://github.com/cyuria/build.nvim',
+	-- 'https://github.com/cyuria/build.nvim',
+	'/home/cyuria/code/build.nvim',
 	'https://github.com/mvllow/modes.nvim',
 	'https://github.com/nvim-mini/mini.nvim',
 	'https://github.com/nvim-treesitter/nvim-treesitter',
@@ -29,10 +33,22 @@ vim.pack.add({
 	'https://github.com/tpope/vim-fugitive',
 })
 
+require('blink.cmp').setup()
+require('build').setup()
 require('mini.git').setup()
 require('mini.icons').setup()
 require('mini.statusline').setup()
-require('modes').setup()
+require('mini.surround').setup({
+	mappings = {
+		add = '<leader>sa',
+		delete = '<leader>sd',
+		find = '<leader>sf',
+		find_left = '<leader>sF',
+		highlight = '<leader>sh',
+		replace = '<leader>sr',
+	},
+})
+require('modes').setup({
+	line_opacity = { visual = 0.4 },
+})
 require('oil').setup()
-require('blink.cmp').setup()
-require('build').setup()
